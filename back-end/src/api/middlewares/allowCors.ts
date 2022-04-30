e0d8949464e119eeda6fa11f5036a9eaddb3d5e2
Brian Leonardo de Morais <morais.brian360@gmail.com>
2022-04-30 12:33:44 -0300

    Adicionado parâmetro Authorization no middleware allowCors

--- back-end/src/api/middlewares/allowCors.ts
+++ back-end/src/api/middlewares/allowCors.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 const allowCors = (req: Request, res: Response, next: NextFunction) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization');
     next();
 }
 
